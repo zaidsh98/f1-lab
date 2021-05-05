266f0a2ac2eeca6f057e99c7a1b33477158550de
sha7rouri <>
2021-05-05 11:55:25 +0000

    adding js file

---
+++ app.js
@@ -0,0 +1,30 @@
+var userName = prompt('Whats Your Name')
+alert('Welcome '+ userName)
+
+var age = prompt("Enter Your Age")
+if (age >=23 && age < 30 ){
+  alert("You are born in the 90's")
+}
+else if (age >=30 && age < 40){
+alert("You are born in the 80's")
+}
+else{
+  alert("You are old to read this")
+}
+
+
+
+
+
+var hour = prompt('what is the clock in 24 houres ??')
+if (hour >=6 && hour <12){
+  alert('Good Morning')
+}
+else if (hour >=12 && hour < 18){
+  alert('Good AfterNoon')
+}
+else {
+alert('Good Evning')
+}
+
+confirm("is your name "+ userName + " ?")
